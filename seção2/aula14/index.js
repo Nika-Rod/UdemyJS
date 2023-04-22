fd6252fb95de0bb8fb056229f4098ead204ba5df
Veronica Rodrigues <veh.rod@gmail.com>
2023-04-22 16:03:25 -0300

    commit

---
+++ seção2/aula14/index.js
@@ -0,0 +1,20 @@
+/* Contastantes
+
+* Não se pode modificar o valor de uma constante 
+
+* As variáveis precisam ter nomes significativos, o nome precisa
+fazer sentido no código
+
+* Não se pode criar variáveis com palavras reservadas da línguagem
+
+* Utilizar camelCase
+
+* As variáveis são case-sensitive.
+
+* Uma constante precisa ser criada e inicializada ao mesmo tempo
+
+*/
+
+const nome = 'Verônica';
+console.log(nome);
+
