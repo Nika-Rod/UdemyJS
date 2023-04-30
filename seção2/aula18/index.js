75a1c0e3e0f72a9b34924631a23d037469793ffd
Veronica Rodrigues <veh.rod@gmail.com>
2023-04-29 21:30:35 -0300

    difference of var and let

---
+++ seção2/aula18/index.js
@@ -0,0 +1,14 @@
+//Diferença entre var e let
+
+/*Var pode ser redeclarada e Let não 
+
+*Let foi criada para resolver alguns problemas que var tinha ao
+criar variáveis
+
+* Criar uma variável sem usar const, let ou var. Irá criar uma váriavel
+global no sistema e futuramente pode atrapalhar no funcionamento
+do código
+
+*Não utilize VAR!
+
+*/
