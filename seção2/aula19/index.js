784f779da7b062a2e6d94b1dd42a6c6441cc0303
Veronica Rodrigues <veh.rod@gmail.com>
2023-04-29 22:26:47 -0300

    primitive data

---
+++ seção2/aula19/index.js
@@ -0,0 +1,19 @@
+/*Dados primitivos
+
+String = texto
+number = número (tanto números inteiros quanto números decimais)
+undefined = váriaveis indefinidas (não possuem nenhum valor)
+null = nulo (não aponta para nenhum lugar da memória)
+boolean = verdadeiro (true) ou falso(false) (valor lógico)
+symbol = ainda não explicado nessa aula, por ser um pouco mais complexo
+
+*/
+
+const firstName = 'Verônica'; //string
+const secondName = "Rodrigues"; //string
+const birthMonth = `Outubro`; //string
+const age = 22; //number
+const height = 1.63; //number
+let empty; //undefined
+const weight = null //null
+const trueOrFalse = true //boolean
